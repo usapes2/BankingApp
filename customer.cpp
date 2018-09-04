2e9078bcf5ca9b55b419a89976eaa7b9a0084a29
ml1976 <mladjo@DESKTOP-U0F53KK.localdomain>
2018-09-03 22:47:58 -0500

    Sorting

--- customer.cpp
+++ customer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <fstream>
 #include <vector>
@@ -43,6 +44,17 @@ class Transaction{
 		int get_id() const {return id;}
 		double get_amount() {return amount;}
 };
+
+bool compare(Customer c1, Customer c2){
+
+		return c1.get_name() < c2.get_name();
+}
+
+bool compare_balance(Customer c1, Customer c2){
+		return c1.get_balance() > c2.get_balance(); 
+
+}
+
 string chopper(string& buffer) {
 
 		string::size_type L = buffer.length(); // This is the length of the line
@@ -61,6 +73,13 @@ void print_Rejected(Transaction t) {
 void print_Invalid(Transaction t) {
 	cout << "Invalid Tx: "<< t.get_id() <<","<<t.get_amount()<<endl;
 }
+
+void print_customer(vector<Customer> v){
+	for(int i=0; i<v.size(); i++){
+		cout << v[i].get_name() << " " << v[i].get_id() << " " << v[i].get_balance() << endl;
+}
+}
+
 int main(){
 
 	string buffer;
@@ -68,8 +87,8 @@ int main(){
 	
 	vector<Customer> v;
 	
-	ifstream in("/mnt/c/Users/acagu/Desktop/Projects/BankingApp/workingFiles/customers01.csv");
-//	ifstream in("/mnt/c/Users/mladjo/Desktop/Projects/BankingApp/workingFiles/customers01.csv");
+//	ifstream in("/mnt/c/Users/acagu/Desktop/Projects/BankingApp/workingFiles/customers01.csv");
+	ifstream in("/mnt/c/Users/mladjo/Desktop/Projects/BankingApp/workingFiles/customers01.csv");
 	while (getline(in,buffer,'\n')) 
 	{
 		string first = chopper(buffer);	
@@ -80,13 +99,11 @@ int main(){
 		v.push_back(cust);
 
 	} 
-
-
-	for(int i=0; i<v.size(); i++){
-		cout << v[i].get_id() << " " << v[i].get_balance() << " " << v[i].get_name() << endl;
-}
-	ifstream intrans("/mnt/c/Users/acagu/Desktop/Projects/BankingApp/workingFiles/transactions01.csv");
-	//ifstream intrans("/mnt/c/Users/mladjo/Desktop/Projects/BankingApp/workingFiles/transactions01.csv");
+	print_customer(v);
+	sort(v.begin(), v.end(), compare);
+	
+//	ifstream intrans("/mnt/c/Users/acagu/Desktop/Projects/BankingApp/workingFiles/transactions01.csv");
+	ifstream intrans("/mnt/c/Users/mladjo/Desktop/Projects/BankingApp/workingFiles/transactions01.csv");
 cout<<"Processing..."<<endl;
 	vector<Transaction> vtrans;
 	while (getline(intrans,buffer,'\n')) 
@@ -113,33 +130,10 @@ cout<<"Processing..."<<endl;
 			} 
 		}
 		if ( trigger == false ) print_Invalid(vtrans[i]);
-	      	
-
-
 	}	
 
-
-
-
-	/*for ( int i = 0 ; i < vtrans.size() ; i++ ) 
-		cout << vtrans[i].get_id() << " " << vtrans[i].get_amount() << endl; */
-
-/* // Testing Customer Class
-	Customer c1(123,222, "mladjo");
-		
-	cout << c1.get_balance() << endl;
-	c1.set_balance(1000000);
-
-	*/
-	for(int i=0; i<v.size(); i++){
-		cout << v[i].get_id() << " " << v[i].get_balance() << " " << v[i].get_name() << endl;
-}
- /* Testing Transaction class
- 	Transaction trans(123, 34.43);
+	sort(v.begin(), v.end(), compare_balance;
+	print_customer(v);
 	
-	cout << trans.get_amount() << endl;
-*/	
-
-
 	return 0;
 }
